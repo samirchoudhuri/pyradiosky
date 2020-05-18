4b20d0fb10ae67a04a3e2da4ceb3b7e22864ed52
bhazelton <bryna.hazelton@gmail.com>
2020-05-18 23:25:09 +0000

    Deploying to gh-pages from  @ 2ee63f07736d5dd0fd07ef94d8a3dab0ba967656 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+gd027f86.head',
+    VERSION: '0.1.dev1+g2ee63f0.add.rtd',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
