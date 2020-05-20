3588131ab41d3656a0bc65cf390085b5249075fa
bhazelton <bhazelton@users.noreply.github.com>
2020-05-20 00:12:43 +0000

    Deploying to gh-pages from  @ 8e05d477af392ad01f2f83824ab3704c45701431 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+gc33d782.add.rtd',
+    VERSION: '0.1.dev1+g8e05d47.head',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
